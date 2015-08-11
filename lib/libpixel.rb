28aaaa3e92deeb53bb97062478f14025c33c2365
Joao Carlos <mail@joao-carlos.com>
2015-08-11 20:20:37 +0300

    Add default client

--- lib/libpixel.rb
+++ lib/libpixel.rb
@@ -2,4 +2,19 @@ require "libpixel/version"
 require "libpixel/client"
 
 module LibPixel
+
+  @@default_client = Client.new
+
+  def self.setup
+    yield @@default_client
+  end
+
+  def self.sign(uri)
+    @@default_client.sign(uri)
+  end
+
+  def self.url(path, options={})
+    @@default_client.url(path, options)
+  end
+
 end
